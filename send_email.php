a303412e93eca3bb96b91f1695b4502dee40705a
pernst <prestonernst30@gmail.com>
2023-11-21 22:14:40 +0100

    fixed subject

--- send_email.php
+++ send_email.php
@@ -5,12 +5,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST') {
     $message = $_POST['message'];
 
     // Format the email body
-    $subject = "Wedding Response from : ";
-    $body = "Name: " . $name . "\n\n" . "Message: " . $message;
+    $subject = "Wedding Response from : " $name;
+    $body = "Name: " . $name . "\n\n" . "Message: " .$message;
 
     // Send the email
     $to = "peslider@gmail.com";
-    $headers = "From: " . $name . "\r\n";
+    $headers = "From: " .$name . "\r\n";
 
     if(mail($to, $subject, $body, $headers)) {
         echo "Email sent successfully!";

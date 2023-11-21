b796dd84ecbb505f355c1b03bdbd39a49898d087
pernst <prestonernst30@gmail.com>
2023-11-21 22:05:08 +0100

    added send_email.php, modified index.html with names in name, eamil. message fields

---
+++ send_email.php
@@ -0,0 +1,21 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+	$email = $_POST['email'];
+    $message = $_POST['message'];
+
+    // Format the email body
+    $subject = "Wedding Response from : ";
+    $body = "Name: " . $name . "\n\n" . "Message: " . $message;
+
+    // Send the email
+    $to = "peslider@gmail.com";
+    $headers = "From: " . $name . "\r\n";
+
+    if(mail($to, $subject, $body, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Error sending email. Please try again.";
+    }
+}
+?>
